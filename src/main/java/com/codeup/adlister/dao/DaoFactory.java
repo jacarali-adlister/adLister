a68458e77079fad18649f73bff52be8079282bb5
CarsonBelew <carbelew@gmail.com>
2019-06-18 12:20:34 -0500

    eh

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -3,7 +3,7 @@ package com.codeup.adlister.dao;
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
-    private static Config config = new Config();
+    private static com.codeup.adLister.models.Config config = new com.codeup.adLister.models.Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -7,7 +7,7 @@ import java.sql.*;
 
 public class MySQLUsersDao implements Users {
     private Connection connection;
-    public MySQLUsersDao(Config config) {
+    public MySQLUsersDao(com.codeup.adLister.models.Config config) {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
